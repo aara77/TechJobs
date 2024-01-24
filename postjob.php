3f9748f831a2ab608a1d8d76fa09cd4585c3fc4c
Your Name <aaratirai777@gmail,com>
2024-01-24 08:30:17 +0545

    changes

--- postjob.php
+++ postjob.php
@@ -118,8 +118,8 @@ include("connect.php");
                <label>Company Description:</label><br>
                <textarea id="description" name="des" rows="5" cols="60" > </textarea>
              </div>
-             <button type="submit">Submit</button>
-             <!-- <input type="submit" name="submit" class="submit" value="Submit"> -->
+             <!-- <button type="submit">Submit</button> -->
+             <input type="submit" name="submit" class="submit" value="Submit">
              
         </form> 
 
